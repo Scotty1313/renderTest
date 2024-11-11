191bbcd494c203b89be06195952e6ba5693711e4
Scotty1313 <63591088+Scotty1313@users.noreply.github.com>
2024-11-11 11:58:43 +0100

    Add files via upload

---
+++ middleware/caching.js
@@ -0,0 +1,11 @@
+var client = require('../redis.js');
+async function cache(req, res, next) {
+  const data = await client.get(req.originalUrl);
+  if (data !== null) {
+    res.render('tutorials', { tutorials: JSON.parse(data) });
+  } else {
+    next();
+  }
+}
+
+module.exports = cache;
